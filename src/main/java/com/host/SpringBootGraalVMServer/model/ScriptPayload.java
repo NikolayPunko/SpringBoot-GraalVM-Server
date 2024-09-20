4ae56b93c5e2256450ffb9df855ea46ff7fe28a9
Nikolay Punko <punkon2001@gmail.com>
2024-09-20 11:21:49 +0300

    Тест доступа к java объекту

--- src/main/java/com/host/SpringBootGraalVMServer/model/ScriptPayload.java
+++ src/main/java/com/host/SpringBootGraalVMServer/model/ScriptPayload.java
@@ -4,15 +4,19 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.graalvm.polyglot.HostAccess;
 
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
 @Setter
-public class ScriptPayload {
 
+public class ScriptPayload {
+    @HostAccess.Export //требуется, чтобы сделать метод доступным из языка встраивания.
     String request;
+    @HostAccess.Export //требуется, чтобы сделать метод доступным из языка встраивания.
     String response;
+    @HostAccess.Export //требуется, чтобы сделать метод доступным из языка встраивания.
     String connection;
 
 }
